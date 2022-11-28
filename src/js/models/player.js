0bde5d633ea143020f18609e33eb776178f555e7
trantuvan <trantuvan.kan@gmail.com>
2022-11-28 13:30:24 +0700

    add comment for AI generateCoord

--- src/js/models/player.js
+++ src/js/models/player.js
@@ -7,13 +7,16 @@ export default function player({ name, isAI = false } = {}) {
     let AIMoves = [];
     const columnLetters = 'ABCDEFGHIJ';
 
+    // *generate all 100 moves from 10x10 grid
     for (let col = 0; col <= columnLetters.length - 1; col++) {
       for (let row = 0; row <= 9; row++) {
         AIMoves.push(`${columnLetters[col]}${row}`);
       }
     }
+    // * shuffle all elements in array to create random moves
     AIMoves = shuffle(AIMoves);
 
+    // *return last move in array, this will ensure no same move return
     return AIMoves.pop();
   }
 
