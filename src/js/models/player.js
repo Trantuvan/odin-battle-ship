bb8f220aa3afbf20a746a8d0cb8121a377716b0c
trantuvan <trantuvan.kan@gmail.com>
2022-11-28 11:58:20 +0700

    improve AImoves so it won't make the same move tiwce

--- src/js/models/player.js
+++ src/js/models/player.js
@@ -1,19 +1,28 @@
+/* eslint-disable no-plusplus */
 import PubSub from 'pubsub-js';
-import getRandomInt from '../utils/getRandomInt';
+import shuffle from '../utils/shuffle';
 
 export default function player({ name, isAI = false } = {}) {
   function generateCoord() {
-    const column = 'ABCDEFGHIJ';
-    const randomRow = getRandomInt(0, 9);
-    const randomCol = column[getRandomInt(0, column.length - 1)];
+    let AIMoves = [];
+    const columnLetters = 'ABCDEFGHIJ';
 
-    return `${randomCol}${randomRow}`;
+    for (let col = 0; col <= columnLetters.length - 1; col++) {
+      for (let row = 0; row <= 9; row++) {
+        AIMoves.push(`${columnLetters[col]}${row}`);
+      }
+    }
+    AIMoves = shuffle(AIMoves);
+
+    return AIMoves.pop();
   }
 
   function shoot(coord) {
     if (isAI === true) {
       const coordAI = generateCoord();
-      PubSub.publish('player.shoot', coordAI);
+      if (coordAI !== undefined) {
+        PubSub.publish('player.shoot', coordAI);
+      }
     } else {
       PubSub.publish('player.shoot', coord);
     }
