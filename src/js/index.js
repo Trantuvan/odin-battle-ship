6289152f9b0750689eff152d90dc19fe3cc78999
trantuvan <trantuvan.kan@gmail.com>
2022-11-30 20:52:25 +0700

    get playerName

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,31 @@ import '../assets/css/style.css';
 import gameController from './controllers/gameController';
 
 (() => {
-  const game = gameController();
-  game.init();
+  const welcomeSection = document.querySelector('.welcome');
+  const formPlayer = document.querySelector('#form-player');
+  const playerNameInput = formPlayer.querySelector('#player-name');
+  const formMessage = formPlayer.querySelector('.form-message');
+  let playerName;
+
+  // *remove error messages
+  playerNameInput.addEventListener('keydown', () => {
+    if (formMessage.classList.contains('disabled') === false) {
+      formMessage.classList.add('disabled');
+    }
+  });
+
+  // *get player name
+  formPlayer.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+
+    if (playerName === undefined || playerName.trim().length === 0) {
+      formMessage.classList.remove('disabled');
+      return undefined;
+    }
+
+    playerName = playerNameInput.value;
+    formPlayer.reset();
+    welcomeSection.classList.add('fadeOut');
+    return undefined;
+  });
 })();
