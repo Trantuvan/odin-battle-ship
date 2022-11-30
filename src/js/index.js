63a1e51f5b18faefbf5fa17eadcacef436c1003a
trantuvan <trantuvan.kan@gmail.com>
2022-11-30 15:35:29 +0700

    import css link

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,9 @@
+// CSS
+import '../assets/css/normalize.css';
+import '../assets/css/reset.css';
+import '../assets/css/style.css';
+
+// JS
 import gameController from './controllers/gameController';
 
 (() => {
