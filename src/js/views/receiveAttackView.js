7e39b54978259b7098f3dd19368b97c95a102948
trantuvan <trantuvan.kan@gmail.com>
2022-12-08 17:56:24 +0700

    fix typo for html

--- src/js/views/receiveAttackView.js
+++ src/js/views/receiveAttackView.js
@@ -97,7 +97,7 @@ export default (function receiveAttackView() {
 
         if (cpuResult.isMiss === true) {
           renderCpuShot(
-            1000,
+            1200,
             missImgCpu,
             cpuResult.coord,
             'the enemy fires a shot at your water and miss'
@@ -106,7 +106,7 @@ export default (function receiveAttackView() {
 
         if (cpuResult.isHit === true) {
           renderCpuShot(
-            1000,
+            1200,
             hitImgCpu,
             cpuResult.coord,
             'the enemy fires a shot at your water and hit'
@@ -141,106 +141,106 @@ export default (function receiveAttackView() {
     <div class="friendly-water">
       <h2 class="title">FRIENDLY WATER</h2>
       <div class="grid">
-        <div class="cell" local-data="A0" isPopulated-data="false"></div>
-        <div class="cell" local-data="B0" isPopulated-data="false"></div>
-        <div class="cell" local-data="C0" isPopulated-data="false"></div>
-        <div class="cell" local-data="D0" isPopulated-data="false"></div>
-        <div class="cell" local-data="E0" isPopulated-data="false"></div>
-        <div class="cell" local-data="F0" isPopulated-data="false"></div>
-        <div class="cell" local-data="G0" isPopulated-data="false"></div>
-        <div class="cell" local-data="H0" isPopulated-data="false"></div>
-        <div class="cell" local-data="I0" isPopulated-data="false"></div>
-        <div class="cell" local-data="J0" isPopulated-data="false"></div>
-        <div class="cell" local-data="A1" isPopulated-data="false"></div>
-        <div class="cell" local-data="B1" isPopulated-data="false"></div>
-        <div class="cell" local-data="C1" isPopulated-data="false"></div>
-        <div class="cell" local-data="D1" isPopulated-data="false"></div>
-        <div class="cell" local-data="E1" isPopulated-data="false"></div>
-        <div class="cell" local-data="F1" isPopulated-data="false"></div>
-        <div class="cell" local-data="G1" isPopulated-data="false"></div>
-        <div class="cell" local-data="H1" isPopulated-data="false"></div>
-        <div class="cell" local-data="I1" isPopulated-data="false"></div>
-        <div class="cell" local-data="J1" isPopulated-data="false"></div>
-        <div class="cell" local-data="A2" isPopulated-data="false"></div>
-        <div class="cell" local-data="B2" isPopulated-data="false"></div>
-        <div class="cell" local-data="C2" isPopulated-data="false"></div>
-        <div class="cell" local-data="D2" isPopulated-data="false"></div>
-        <div class="cell" local-data="E2" isPopulated-data="false"></div>
-        <div class="cell" local-data="F2" isPopulated-data="false"></div>
-        <div class="cell" local-data="G2" isPopulated-data="false"></div>
-        <div class="cell" local-data="H2" isPopulated-data="false"></div>
-        <div class="cell" local-data="I2" isPopulated-data="false"></div>
-        <div class="cell" local-data="J2" isPopulated-data="false"></div>
-        <div class="cell" local-data="A3" isPopulated-data="false"></div>
-        <div class="cell" local-data="B3" isPopulated-data="false"></div>
-        <div class="cell" local-data="C3" isPopulated-data="false"></div>
-        <div class="cell" local-data="D3" isPopulated-data="false"></div>
-        <div class="cell" local-data="E3" isPopulated-data="false"></div>
-        <div class="cell" local-data="F3" isPopulated-data="false"></div>
-        <div class="cell" local-data="G3" isPopulated-data="false"></div>
-        <div class="cell" local-data="H3" isPopulated-data="false"></div>
-        <div class="cell" local-data="I3" isPopulated-data="false"></div>
-        <div class="cell" local-data="J3" isPopulated-data="false"></div>
-        <div class="cell" local-data="A4" isPopulated-data="false"></div>
-        <div class="cell" local-data="B4" isPopulated-data="false"></div>
-        <div class="cell" local-data="C4" isPopulated-data="false"></div>
-        <div class="cell" local-data="D4" isPopulated-data="false"></div>
-        <div class="cell" local-data="E4" isPopulated-data="false"></div>
-        <div class="cell" local-data="F4" isPopulated-data="false"></div>
-        <div class="cell" local-data="G4" isPopulated-data="false"></div>
-        <div class="cell" local-data="H4" isPopulated-data="false"></div>
-        <div class="cell" local-data="I4" isPopulated-data="false"></div>
-        <div class="cell" local-data="J4" isPopulated-data="false"></div>
-        <div class="cell" local-data="A5" isPopulated-data="false"></div>
-        <div class="cell" local-data="B5" isPopulated-data="false"></div>
-        <div class="cell" local-data="C5" isPopulated-data="false"></div>
-        <div class="cell" local-data="D5" isPopulated-data="false"></div>
-        <div class="cell" local-data="E5" isPopulated-data="false"></div>
-        <div class="cell" local-data="F5" isPopulated-data="false"></div>
-        <div class="cell" local-data="G5" isPopulated-data="false"></div>
-        <div class="cell" local-data="H5" isPopulated-data="false"></div>
-        <div class="cell" local-data="I5" isPopulated-data="false"></div>
-        <div class="cell" local-data="J5" isPopulated-data="false"></div>
-        <div class="cell" local-data="A6" isPopulated-data="false"></div>
-        <div class="cell" local-data="B6" isPopulated-data="false"></div>
-        <div class="cell" local-data="C6" isPopulated-data="false"></div>
-        <div class="cell" local-data="D6" isPopulated-data="false"></div>
-        <div class="cell" local-data="E6" isPopulated-data="false"></div>
-        <div class="cell" local-data="E6" isPopulated-data="false"></div>
-        <div class="cell" local-data="G6" isPopulated-data="false"></div>
-        <div class="cell" local-data="H6" isPopulated-data="false"></div>
-        <div class="cell" local-data="I6" isPopulated-data="false"></div>
-        <div class="cell" local-data="J6" isPopulated-data="false"></div>
-        <div class="cell" local-data="A7" isPopulated-data="false"></div>
-        <div class="cell" local-data="B7" isPopulated-data="false"></div>
-        <div class="cell" local-data="C7" isPopulated-data="false"></div>
-        <div class="cell" local-data="D7" isPopulated-data="false"></div>
-        <div class="cell" local-data="E7" isPopulated-data="false"></div>
-        <div class="cell" local-data="F7" isPopulated-data="false"></div>
-        <div class="cell" local-data="G7" isPopulated-data="false"></div>
-        <div class="cell" local-data="H7" isPopulated-data="false"></div>
-        <div class="cell" local-data="I7" isPopulated-data="false"></div>
-        <div class="cell" local-data="J7" isPopulated-data="false"></div>
-        <div class="cell" local-data="A8" isPopulated-data="false"></div>
-        <div class="cell" local-data="B8" isPopulated-data="false"></div>
-        <div class="cell" local-data="C8" isPopulated-data="false"></div>
-        <div class="cell" local-data="D8" isPopulated-data="false"></div>
-        <div class="cell" local-data="E8" isPopulated-data="false"></div>
-        <div class="cell" local-data="F8" isPopulated-data="false"></div>
-        <div class="cell" local-data="G8" isPopulated-data="false"></div>
-        <div class="cell" local-data="H8" isPopulated-data="false"></div>
-        <div class="cell" local-data="I8" isPopulated-data="false"></div>
-        <div class="cell" local-data="J8" isPopulated-data="false"></div>
-        <div class="cell" local-data="A9" isPopulated-data="false"></div>
-        <div class="cell" local-data="B9" isPopulated-data="false"></div>
-        <div class="cell" local-data="C9" isPopulated-data="false"></div>
-        <div class="cell" local-data="D9" isPopulated-data="false"></div>
-        <div class="cell" local-data="E9" isPopulated-data="false"></div>
-        <div class="cell" local-data="F9" isPopulated-data="false"></div>
-        <div class="cell" local-data="G9" isPopulated-data="false"></div>
-        <div class="cell" local-data="H9" isPopulated-data="false"></div>
-        <div class="cell" local-data="I9" isPopulated-data="false"></div>
-        <div class="cell" local-data="J9" isPopulated-data="false"></div>
+      <div class="cell" local-data="A0" isPopulated-data="false"></div>
+      <div class="cell" local-data="B0" isPopulated-data="false"></div>
+      <div class="cell" local-data="C0" isPopulated-data="false"></div>
+      <div class="cell" local-data="D0" isPopulated-data="false"></div>
+      <div class="cell" local-data="E0" isPopulated-data="false"></div>
+      <div class="cell" local-data="F0" isPopulated-data="false"></div>
+      <div class="cell" local-data="G0" isPopulated-data="false"></div>
+      <div class="cell" local-data="H0" isPopulated-data="false"></div>
+      <div class="cell" local-data="I0" isPopulated-data="false"></div>
+      <div class="cell" local-data="J0" isPopulated-data="false"></div>
+      <div class="cell" local-data="A1" isPopulated-data="false"></div>
+      <div class="cell" local-data="B1" isPopulated-data="false"></div>
+      <div class="cell" local-data="C1" isPopulated-data="false"></div>
+      <div class="cell" local-data="D1" isPopulated-data="false"></div>
+      <div class="cell" local-data="E1" isPopulated-data="false"></div>
+      <div class="cell" local-data="F1" isPopulated-data="false"></div>
+      <div class="cell" local-data="G1" isPopulated-data="false"></div>
+      <div class="cell" local-data="H1" isPopulated-data="false"></div>
+      <div class="cell" local-data="I1" isPopulated-data="false"></div>
+      <div class="cell" local-data="J1" isPopulated-data="false"></div>
+      <div class="cell" local-data="A2" isPopulated-data="false"></div>
+      <div class="cell" local-data="B2" isPopulated-data="false"></div>
+      <div class="cell" local-data="C2" isPopulated-data="false"></div>
+      <div class="cell" local-data="D2" isPopulated-data="false"></div>
+      <div class="cell" local-data="E2" isPopulated-data="false"></div>
+      <div class="cell" local-data="F2" isPopulated-data="false"></div>
+      <div class="cell" local-data="G2" isPopulated-data="false"></div>
+      <div class="cell" local-data="H2" isPopulated-data="false"></div>
+      <div class="cell" local-data="I2" isPopulated-data="false"></div>
+      <div class="cell" local-data="J2" isPopulated-data="false"></div>
+      <div class="cell" local-data="A3" isPopulated-data="false"></div>
+      <div class="cell" local-data="B3" isPopulated-data="false"></div>
+      <div class="cell" local-data="C3" isPopulated-data="false"></div>
+      <div class="cell" local-data="D3" isPopulated-data="false"></div>
+      <div class="cell" local-data="E3" isPopulated-data="false"></div>
+      <div class="cell" local-data="F3" isPopulated-data="false"></div>
+      <div class="cell" local-data="G3" isPopulated-data="false"></div>
+      <div class="cell" local-data="H3" isPopulated-data="false"></div>
+      <div class="cell" local-data="I3" isPopulated-data="false"></div>
+      <div class="cell" local-data="J3" isPopulated-data="false"></div>
+      <div class="cell" local-data="A4" isPopulated-data="false"></div>
+      <div class="cell" local-data="B4" isPopulated-data="false"></div>
+      <div class="cell" local-data="C4" isPopulated-data="false"></div>
+      <div class="cell" local-data="D4" isPopulated-data="false"></div>
+      <div class="cell" local-data="E4" isPopulated-data="false"></div>
+      <div class="cell" local-data="F4" isPopulated-data="false"></div>
+      <div class="cell" local-data="G4" isPopulated-data="false"></div>
+      <div class="cell" local-data="H4" isPopulated-data="false"></div>
+      <div class="cell" local-data="I4" isPopulated-data="false"></div>
+      <div class="cell" local-data="J4" isPopulated-data="false"></div>
+      <div class="cell" local-data="A5" isPopulated-data="false"></div>
+      <div class="cell" local-data="B5" isPopulated-data="false"></div>
+      <div class="cell" local-data="C5" isPopulated-data="false"></div>
+      <div class="cell" local-data="D5" isPopulated-data="false"></div>
+      <div class="cell" local-data="E5" isPopulated-data="false"></div>
+      <div class="cell" local-data="F5" isPopulated-data="false"></div>
+      <div class="cell" local-data="G5" isPopulated-data="false"></div>
+      <div class="cell" local-data="H5" isPopulated-data="false"></div>
+      <div class="cell" local-data="I5" isPopulated-data="false"></div>
+      <div class="cell" local-data="J5" isPopulated-data="false"></div>
+      <div class="cell" local-data="A6" isPopulated-data="false"></div>
+      <div class="cell" local-data="B6" isPopulated-data="false"></div>
+      <div class="cell" local-data="C6" isPopulated-data="false"></div>
+      <div class="cell" local-data="D6" isPopulated-data="false"></div>
+      <div class="cell" local-data="E6" isPopulated-data="false"></div>
+      <div class="cell" local-data="F6" isPopulated-data="false"></div>
+      <div class="cell" local-data="G6" isPopulated-data="false"></div>
+      <div class="cell" local-data="H6" isPopulated-data="false"></div>
+      <div class="cell" local-data="I6" isPopulated-data="false"></div>
+      <div class="cell" local-data="J6" isPopulated-data="false"></div>
+      <div class="cell" local-data="A7" isPopulated-data="false"></div>
+      <div class="cell" local-data="B7" isPopulated-data="false"></div>
+      <div class="cell" local-data="C7" isPopulated-data="false"></div>
+      <div class="cell" local-data="D7" isPopulated-data="false"></div>
+      <div class="cell" local-data="E7" isPopulated-data="false"></div>
+      <div class="cell" local-data="F7" isPopulated-data="false"></div>
+      <div class="cell" local-data="G7" isPopulated-data="false"></div>
+      <div class="cell" local-data="H7" isPopulated-data="false"></div>
+      <div class="cell" local-data="I7" isPopulated-data="false"></div>
+      <div class="cell" local-data="J7" isPopulated-data="false"></div>
+      <div class="cell" local-data="A8" isPopulated-data="false"></div>
+      <div class="cell" local-data="B8" isPopulated-data="false"></div>
+      <div class="cell" local-data="C8" isPopulated-data="false"></div>
+      <div class="cell" local-data="D8" isPopulated-data="false"></div>
+      <div class="cell" local-data="E8" isPopulated-data="false"></div>
+      <div class="cell" local-data="F8" isPopulated-data="false"></div>
+      <div class="cell" local-data="G8" isPopulated-data="false"></div>
+      <div class="cell" local-data="H8" isPopulated-data="false"></div>
+      <div class="cell" local-data="I8" isPopulated-data="false"></div>
+      <div class="cell" local-data="J8" isPopulated-data="false"></div>
+      <div class="cell" local-data="A9" isPopulated-data="false"></div>
+      <div class="cell" local-data="B9" isPopulated-data="false"></div>
+      <div class="cell" local-data="C9" isPopulated-data="false"></div>
+      <div class="cell" local-data="D9" isPopulated-data="false"></div>
+      <div class="cell" local-data="E9" isPopulated-data="false"></div>
+      <div class="cell" local-data="F9" isPopulated-data="false"></div>
+      <div class="cell" local-data="G9" isPopulated-data="false"></div>
+      <div class="cell" local-data="H9" isPopulated-data="false"></div>
+      <div class="cell" local-data="I9" isPopulated-data="false"></div>
+      <div class="cell" local-data="J9" isPopulated-data="false"></div>
       </div>
     </div>
     <div class="enemy-water">
@@ -311,7 +311,7 @@ export default (function receiveAttackView() {
         <div class="cell" local-data="C6" isPopulated-data="false"></div>
         <div class="cell" local-data="D6" isPopulated-data="false"></div>
         <div class="cell" local-data="E6" isPopulated-data="false"></div>
-        <div class="cell" local-data="E6" isPopulated-data="false"></div>
+        <div class="cell" local-data="F6" isPopulated-data="false"></div>
         <div class="cell" local-data="G6" isPopulated-data="false"></div>
         <div class="cell" local-data="H6" isPopulated-data="false"></div>
         <div class="cell" local-data="I6" isPopulated-data="false"></div>
